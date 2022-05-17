71f3aabd32e9df38179b495b075d6b128b12024b
Bruno Rossini <bruno.rossini@banqi.com>
2022-05-17 09:29:47 -0300

    implementa ! ao final da mensagem de log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function parOuImpar(x) {
-  console.log('Log de início de função')
+  console.log('Log de início de função!')
   if (x % 2 == 0) return console.log('O Número é Par')
 
   return console.log('O Número é Ímpar')
